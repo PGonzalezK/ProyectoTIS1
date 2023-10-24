a11f4b362e200c14cca3fb46bb08146c3fcdf32a
nparrac1 <jmunozv@ing.ucsc.cl>
2023-10-24 14:13:50 -0300

    Avance Interfaz
    pagina de inicio en home.php mas arreglo de footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -1,4 +1,4 @@
-<div class="container fixed-bottom ">
+<div class="container-bottom ">
     <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
         <div class="col d-flex align-items-center">
             <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
@@ -8,7 +8,7 @@
             </a>
 
 
-            <div class="container text-center">
+            <!-- <div class="container text-center">
                 <div class="row">
                     <div class="col-3">
                         <span class="mb-3 mb-md-0 text-body-secondary">Seguridad Ciudadana: +56 9 4569 2347</span>
@@ -24,36 +24,88 @@
                         <span class="mb-3 mb-md-0 text-body-secondary">Alumbrado publico: +56 41 239 4723</span>
                     </div>
                 </div>
+            </div> -->
+            <div class="container d-flex flex-column justify-content-center align-items-center">
+                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
+                    <div class="col">
+                        <span class="mb-3 text-body-secondary">Seguridad Ciudadana: +56 9 4569 2347</span>
+                    </div>
+
+                    <div class="col">
+                        <span class="mb-3 text-body-secondary">Mesa central: 800 816 00</span>
+                    </div>
+
+                    <div class="col">
+                        <span class="mb-3 text-body-secondary">Direccion: Ejercito 1030. Concepcion, Chile</span>
+                    </div>
+
+                    <div class="col">
+                        <span class="mb-3 text-body-secondary">Emergencia: *4142</span>
+                    </div>
+
+                    <div class="col">
+                        <span class="mb-3 text-body-secondary">Nexo municipal: +56 41 298 7672</span>
+                    </div>
+
+                    <div class="col">
+                        <span class="mb-3 text-body-secondary">Alumbrado público: +56 41 239 4723</span>
+                    </div>
+                </div>
+
             </div>
 
 
         </div>
 
-        <ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
-            <li class="ms-3">
-                <a class="text-body-secondary" href="#">
-                    <i class="fa-brands fa-x-twitter"></i>
-                </a>
-            </li>
-            <li class="ms-3">
-                <a class="text-body-secondary" href="#">
-                    <i class="fa-brands fa-facebook"></i>
-                </a>
-            </li>
-            <li class="ms-3">
-                <a class="text-body-secondary" href="#">
-                    <i class="fa-brands fa-instagram"></i>
-                </a>
-            </li>
-        </ul>
-    </footer>
+
+
+</div>
+
+<!--<ul class="nav col-md-4 justify-content-end list-unstyled d-flex">
+    <li class="ms-3">
+        <a class="text-body-secondary" href="#">
+            <i class="fa-brands fa-x-twitter"></i>
+        </a>
+    </li>
+    <li class="ms-3">
+        <a class="text-body-secondary" href="#">
+            <i class="fa-brands fa-facebook"></i>
+        </a>
+    </li>
+    <li class="ms-3">
+        <a class="text-body-secondary" href="#">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+    </li>
+</ul> (esto es lo que tenias pablito)-->
+<br>
+<ul class="nav col-md-4 list-unstyled d-flex justify-content-end">
+    <li class="ms-3">
+        <a class="text-body-secondary" href="#">
+            <i class="fa-brands fa-x-twitter"></i>
+        </a>
+    </li>
+    <li class="ms-3">
+        <a class="text-body-secondary" href="#">
+            <i class="fa-brands fa-facebook"></i>
+        </a>
+    </li>
+    <li class="ms-3">
+        <a class="text-body-secondary" href="#">
+            <i class="fa-brands fa-instagram"></i>
+        </a>
+    </li>
+</ul>
+
+
+
+
+</footer>
 </div>
 
 </div>
 <!-- Scripts de Bootstrap -->
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
-    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"
-    crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 
 </body>
 
--- pages/home.php
+++ pages/home.php
@@ -16,8 +16,7 @@
                         <a class="nav-link" href="#">Mapa</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-                            aria-expanded="false" value="dd">Actualidad</a>
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false" value="dd">Actualidad</a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="#">Noticias</a></li>
                             <li><a class="dropdown-item" href="#">Eventos</a></li>
@@ -25,8 +24,7 @@
                         </ul>
                     </li>
                     <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
-                            aria-expanded="false">Nexo Municipal</a>
+                        <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button" aria-expanded="false">Nexo Municipal</a>
                         <ul class="dropdown-menu">
                             <li><a class="dropdown-item" href="#">Mision y Vision</a></li>
                             <li><a class="dropdown-item" href="#">Palabras del alcalde</a></li>
@@ -37,20 +35,98 @@
                         <a class="nav-link" href="#">Parcitipacion</a>
                     </li>
 
-        </div>
-    </div>
-
-
-
-    <div class="px-4 py-5 my-5 text-center">
-        <h1 class="display-5 fw-bold">Nexo Municipal</h1>
-        <div class="col-lg-6 mx-auto">
-            <p class="lead mb-4">Quickly design and customize responsive mobile-first sites with Bootstrap, the world’s
-                most
-                popular front-end open source toolkit, featuring Sass variables and mixins, responsive grid system,
-                extensive prebuilt components, and powerful JavaScript plugins.</p>
-            <div class="d-grid gap-2 d-sm-flex justify-content-sm-center">
-
             </div>
         </div>
+
+
+        <div class="row row-cols-1 row-cols-md-3 g-4">
+            <div class="col">
+                <div class="card h-100">
+                <img src="https://munipuqueldon.cl/wp-content/uploads/2023/07/IMG_4378-scaled.jpg" class="card-img-top" style="height: 238px">
+                    <div class="card-body">
+                    <h5 class="card-title">Municipio de Puqueldón entrega 50 Becas a estudiantes destacados de la comuna.</h5>
+                    <p class="card-text">Durante la mañana del 20 de julio, en el salón principal del Edificio Polifuncional,
+                        se realizó...</p>
+                    </div>
+                    <div class="card-footer">
+                    <small class="text-body-secondary">17 Octubre 2023</small>
+                </div>
+                </div>
+            </div>
+
+            <div class="col">
+                <div class="card h-100">
+                   <img src="https://munipuqueldon.cl/wp-content/uploads/2023/06/DSC_0467-1024x683.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Egresan los primeros miembros del centro diurno del adulto mayor</h5>
+                    <p class="card-text">Este centro, que abrió sus puertas a la comunidad en diciembre del 2021 ...</p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">18 Octubre 2023</small>
+                </div>
+                </div>
+
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://munipuqueldon.cl/wp-content/uploads/2023/06/DSC_0319-1024x683.jpg" class="card-img-top">
+                <div class="card-body">
+                    <h5 class="card-title">Día Mundial del Medioambiente: Un llamado a la protección de los ecosistemas locales</h5>
+                    <p class="card-text">En un esfuerzo conjunto por concientizar sobre la importancia de preservar el medio... </p>
+                </div>
+                <div class="card-footer">
+                    <small class="text-body-secondary">19 Octubre 2023</small>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://munipuqueldon.cl/wp-content/uploads/2023/07/DSC_0003-1024x683.jpg" class="card-img-top" alt="...">
+                    <div class="card-body">
+                    <h5 class="card-title"> Compañia chilota de teatro, tétires y marionetas se presento en la comuna </h5>
+                    <p class="card-text">Con la obra titulada “Libertad de las maravillas”, y luego de una semana...</p>
+                    </div>
+                    <div class="card-footer">
+                    <small class="text-body-secondary">20 Octubre 2023</small>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://munipuqueldon.cl/wp-content/uploads/2023/05/335447144_581250817278770_6242936035129980254_n.jpg" class="card-img-top">
+                    <div class="card-body">
+                        <h5 class="card-title">Operativo de esterilización gratuitafinaliza exitosamente con 500 mascotas operadas</h5>
+                        <p class="card-text">realizaron 500 intervenciones quirúrgicas a mascotas (perros y gatos), todas ellas con resultados exitosos...</p>
+                    </div>
+                    <div class="card-footer">
+                    <small class="text-body-secondary">21 Octubre 2023</small>
+                </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="card h-100">
+                    <img src="https://munipuqueldon.cl/wp-content/uploads/2023/05/xA_UNO_1419483.jpeg.pagespeed.ic_.ebgryL7UHV.jpg" class="card-img-top" style="height: 238px">
+                    <div class="card-body">
+                        <h5 class="card-title">Profesionales del SAG hacen llamado a denunciar posibles casos de Influenza Aviar</h5>
+                        <p class="card-text">profesionales del SAG llegaron a Puqueldón para informar a la comunidad sobre síntomas...</p>
+                    </div>
+                    <div class="card-footer">
+                    <small class="text-body-secondary">22 Octubre 2023</small>
+                </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+
+
+
+
+        <div class="d-grid gap-2 d-sm-flex justify-content">
+
+        </div>
+    </div>
     </div>
