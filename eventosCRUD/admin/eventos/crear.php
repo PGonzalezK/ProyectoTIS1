aba6267e4160098ea72d785c857f44cfae96e7de
Rayen <rayennre@gmail.com>
2023-11-02 00:51:43 -0300

    crudEventos

--- eventosCRUD/admin/eventos/crear.php
+++ eventosCRUD/admin/eventos/crear.php
@@ -1,6 +1,28 @@
 <?php 
     require '../../includes/config/database.php';
     $db = conectarDB();
+
+
+    if($_SERVER['REQUEST_METHOD'] == 'POST'){
+        echo "<pre>";
+        var_dump($_POST);
+        echo "</pre>";
+    }
+
+    $titulo = $_POST['titulo'];
+    $direccion =$_POST['direccion'];
+    $descripcion =$_POST['descripcion'];
+    $idPeriodista = $_POST['idPeriodista'];
+
+    //Insertar Variables
+    $query = "INSERT INTO eventos (titulo , direccion, descripcion, idPeriodista) VALUES ('$titulo','$direccion','$descripcion', '$idPeriodista')";
+    //resultados
+    $resultado = mysqli_query($db,$query);
+
+    if($resultado){
+        echo "insertado correctamente";
+    }
+
     require '../../includes/funciones.php';
     incluirTemplate('header');
 ?>
@@ -30,16 +52,20 @@
                     </div>
 
                     <div class="mb-3">
-                        <label for="descripcionEvento" class="form-label">Descripcion del Evento</label>
-                        <textarea class="form-control" id="exampleFormControlTextarea1" name="descripcionEvento" rows="4"></textarea>
+                        <label class="input-group-text" for="descripcion">DESCRIPCIÓN</label>
+                        <input type="text" class="form-control" name="descripcion" placeholder="escriba una breve descripcion"
+                            required></input>
+                        <div class="invalid-feedback">
+                           ESCRIBA BREVE DESCRIPCION DEL EVENTO
+                        </div>
                     </div>
 
                     <div class="col-md-12 mb-3">
-                        <label for="nombrePeriodista" class="form-label">Nombre Periodista</label>
-                        <select class="form-control" id="origin" name="nombrePeriodista">
-                            <option value="Periodista1">Pablo</option>
-                            <option value="Periodista2">Pablo 2 </option>
-                            <option value="Periodista3">Javiera</option>
+                        <label for="idPeriodista" class="form-label">Nombre Periodista</label>
+                        <select class="form-control" id="origin" name ="idPeriodista">
+                            <option value="1">Pablo</option>
+                            <option value="2">Pablo 2 </option>
+                            <option value="3">Javiera</option>
                         </select>
                     </div>
 
