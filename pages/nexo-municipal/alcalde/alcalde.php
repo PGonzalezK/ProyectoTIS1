c9f788f71daa43a2fccab0bf1de299560c61c7fd
Javiera <142517665+danaemunoz@users.noreply.github.com>
2023-11-03 01:53:45 -0300

    Avance
    barra de navegación de palabras alcalde

--- pages/nexo-municipal/alcalde/alcalde.php
+++ pages/nexo-municipal/alcalde/alcalde.php
@@ -13,7 +13,6 @@ $imagen = $row['imagen'];
 $fecha = $row['fecha'];
 
 ?>
-
 <div class="container text-center">
     <div class="row">
         <div class="col">
@@ -22,7 +21,42 @@ $fecha = $row['fecha'];
 
         <div class="col-12">
             <ul class="nav justify-content-center">
-                <!-- Tus elementos del menú aquí -->
+            <li class="nav-item">
+                    <a class="nav-link" href="index.php?p=home">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a id="emprendedores-link" class="nav-link"
+                        href="index.php?p=emprendedores/emprendedores">Emprendedores</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="index.php?p=mapa/mapa">Mapa</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                        aria-expanded="false" value="dd">Actualidad</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="index.php?p=actualidad/noticias/noticias">Noticias</a></li>
+                        <li><a class="dropdown-item" href="index.php?p=actualidad/eventos/eventos">Eventos</a></li>
+
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                        aria-expanded="false">Nexo Municipal</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="index.php?p=nexo-municipal/misionyvision/misionvision">Mision
+                                y Vision</a></li>
+                        <li><a class="dropdown-item" href="index.php?p=nexo-municipal/alcalde/alcalde">Palabras del
+                                alcalde</a></li>
+                        <li><a class="dropdown-item"
+                                href="index.php?p=nexo-municipal/direccionesmunicipales/direcciones">Direcciones
+                                Municipales</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a id="participacion-link" class="nav-link"
+                        href="index.php?p=participacion\participacion">Participacion</a>
+                </li>
             </ul>
         </div>
     </div>
