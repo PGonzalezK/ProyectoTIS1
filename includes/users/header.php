0221cb00c4156d3804e41b7a9ab48a2ce7e58d76
Pvblo <pmonjes@ing.ucsc.cl>
2023-11-03 00:59:30 -0300

    interfaz
    Co-Authored-By: BigKller <142517757+BigKeller@users.noreply.github.com>
    Co-Authored-By: Javiera <142517665+danaemunoz@users.noreply.github.com>

--- includes/users/header.php
+++ includes/users/header.php
@@ -16,7 +16,7 @@
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
 	<!-- CSS -->
-	<link rel="stylesheet" href="C:\xampp\htdocs\xampp\ProyectoTIS1\pages\auth\styles\login.css">
+	<link rel="stylesheet" href="pages\auth\styles\login.css">
 
 
 </head>
--- pages/nexo-municipal/misionyvision/misionvision.php
+++ pages/nexo-municipal/misionyvision/misionvision.php
@@ -1,5 +1,5 @@
 <?php
-include("middleware/auth.php");
+//include("middleware/auth.php");
 include("database/connection.php");
 
 // Consulta para obtener la Misión y la Visión desde la base de datos
@@ -17,7 +17,54 @@ while ($row = mysqli_fetch_assoc($result)) {
     }
 }
 ?>
+<div class="container text-center">
+    <div class="row">
+        <div class="col">
 
+        </div>
+
+        <div class="col-12">
+            <ul class="nav justify-content-center">
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php?p=home">Inicio</a>
+                </li>
+                <li class="nav-item">
+                    <a id="emprendedores-link" class="nav-link"
+                        href="index.php?p=emprendedores/emprendedores">Emprendedores</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="index.php?p=mapa/mapa">Mapa</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                        aria-expanded="false" value="dd">Actualidad</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="index.php?p=actualidad/noticias/noticias">Noticias</a></li>
+                        <li><a class="dropdown-item" href="index.php?p=actualidad/eventos/eventos">Eventos</a></li>
+
+                    </ul>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" data-bs-toggle="dropdown" href="#" role="button"
+                        aria-expanded="false">Nexo Municipal</a>
+                    <ul class="dropdown-menu">
+                        <li><a class="dropdown-item" href="index.php?p=nexo-municipal/misionyvision/misionvision">Mision
+                                y Vision</a></li>
+                        <li><a class="dropdown-item" href="index.php?p=nexo-municipal/alcalde/alcalde">Palabras del
+                                alcalde</a></li>
+                        <li><a class="dropdown-item"
+                                href="index.php?p=nexo-municipal/direccionesmunicipales/direcciones">Direcciones
+                                Municipales</a></li>
+                    </ul>
+                </li>
+                <li class="nav-item">
+                    <a id="participacion-link" class="nav-link"
+                        href="index.php?p=participacion\participacion">Participacion</a>
+                </li>
+
+        </div>
+    </div>
+    
 <div class="container text-center">
     <div class="mision-backg">
         <br><br>
