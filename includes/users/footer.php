6daac28b067ceb22bdea3ceebaa65a9611630fc7
Rayen <rayennre@gmail.com>
2023-11-03 21:43:49 -0300

    imagen agregada al footer

--- includes/users/footer.php
+++ includes/users/footer.php
@@ -5,44 +5,64 @@
                 <svg class="bi" width="30" height="24">
                     <use xlink:href="#bootstrap"></use>
                 </svg>
-            </a>
+                </a>
+        
+               
+            
 
 
-
-            <div class="container d-flex flex-column justify-content-center align-items-center">
-                <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">
-
-                <div class="col">
-                <img src="pages\auth\images\logo.png" class="float-start" alt="...">
-                </div>
-                
-                    <div class="col">
-                        <span class="mb-3 text-body-secondary">Seguridad Ciudadana: +56 9 4569 2347</span>
+                <div class="row align-items-center">
+                    <div class="col-6 col-sm-3" >
+                            <img src="pages\auth\images\logo.png" class="¿img-fluid rounded" alt="" width="300">
                     </div>
-
                     <div class="col">
-                        <span class="mb-3 text-body-secondary">Mesa central: 800 816 00</span>
-                    </div>
-
-                    <div class="col">
-                        <span class="mb-3 text-body-secondary">Direccion: Ejercito 1030. Concepcion, Chile</span>
-                    </div>
-
-                    <div class="col">
-                        <span class="mb-3 text-body-secondary">Emergencia: *4142</span>
-                    </div>
-
-                    <div class="col">
-                        <span class="mb-3 text-body-secondary">Nexo municipal: +56 41 298 7672</span>
-                    </div>
-
-                    <div class="col">
-                        <span class="mb-3 text-body-secondary">Alumbrado público: +56 41 239 4723</span>
-                    </div>
                     
+                    <div class="container d-flex flex-column justify-content-center align-items-center">
+                        <div class="row row-cols-1 row-cols-md-2 row-cols-lg-3 g-4">            
+                            
+                            <div class="col">
+                                <span class="mb-3 text-body-secondary">Seguridad Ciudadana: +56 9 4569 2347</span>
+                            </div>
+
+                            <div class="col">
+                                <span class="mb-3 text-body-secondary">Mesa central: 800 816 00</span>
+                            </div>
+
+                            <div class="col">
+                                <span class="mb-3 text-body-secondary">Direccion: Ejercito 1030. Concepcion, Chile</span>
+                            </div>
+
+                            <div class="col">
+                                <span class="mb-3 text-body-secondary">Emergencia: *4142</span>
+                            </div>
+
+                            <div class="col">
+                                <span class="mb-3 text-body-secondary">Nexo municipal: +56 41 298 7672</span>
+                            </div>
+
+                            <div class="col">
+                                <span class="mb-3 text-body-secondary">Alumbrado público: +56 41 239 4723</span>
+                            </div>
+                            
+                        </div>
+
+                    </div>
+
+
+                    
+                    </div>
                 </div>
 
-            </div>
+
+
+
+                    
+              
+
+
+
+                 
+                
 
 
         </div>
--- includes/users/header.php
+++ includes/users/header.php
@@ -5,7 +5,8 @@
 	<title>User</title>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	
+		<!-- CSS -->
+		<link rel="stylesheet" href="pages\auth\styles\login.css">
 	<!-- Bootstrap  -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
@@ -15,8 +16,7 @@
 	<!-- jQuery -->
 	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 
-	<!-- CSS -->
-	<link rel="stylesheet" href="pages\auth\styles\login.css">
+
 
 
 </head>
