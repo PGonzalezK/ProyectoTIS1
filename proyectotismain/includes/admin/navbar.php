097c4c68e1a20c8795eb9fddb1f916b668073ee7
BigKeller <pgonzalezk@ing.ucsc.cl>
2023-11-15 23:12:33 -0300

    vuelta atras
    Co-Authored-By: Rayen <64786136+Lexugawa@users.noreply.github.com>
    Co-Authored-By: Pvblo <117694123+Pvbm22@users.noreply.github.com>
    Co-Authored-By: Javiera <142517665+danaemunoz@users.noreply.github.com>

--- proyectotismain/includes/admin/navbar.php
+++ proyectotismain/includes/admin/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom colornav ">
+<nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom ">
     <div class="container-fluid">
         <a class="navbar-brand" href="#"></a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
@@ -6,11 +6,62 @@
         </button>
 
         <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+
             <?php
             if (isset($_SESSION["email"])) {
             ?>
-            <img src="pages\auth\images\logo.png" class="¿img-fluid rounded imagen-nexo" width="200" >
-            <?php } ?>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo ($pagina == 'home') ? 'active' : null ?>" aria-current="page" href="index.php?p=admin/home">Inicio</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo (strpos($pagina, 'users') !== false) ? 'active' : null ?>" href="index.php?p=admin/users/index">Usuarios</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo (strpos($pagina, 'noticias') !== false) ? 'active' : null ?>" href="index.php?p=admin/noticias_adm/index">Noticias</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo (strpos($pagina, 'eventos') !== false) ? 'active' : null ?>" href="index.php?p=admin/eventos_adm/index">Eventos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo (strpos($pagina, 'roles') !== false) ? 'active' : null ?>" href="index.php?p=admin/roles/index">Roles</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo (strpos($pagina, 'misionvision') !== false) ? 'active' : null ?>" href="index.php?p=admin/misionvision/index">Mision y Vision</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo (strpos($pagina, 'palabras_alcalde') !== false) ? 'active' : null ?>" href="index.php?p=admin/palabras_alcalde/index">Palabras del Alcalde</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo (strpos($pagina, 'direccionesMunicipales') !== false) ? 'active' : null ?>" href="index.php?p=admin/direccionesMunicipales/index">Direcciones Municipales</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo (strpos($pagina, 'backgrounds') !== false) ? 'active' : null ?>" href="index.php?p=admin/backgrounds/index">Fondos</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo (strpos($pagina, 'backgrounds') !== false) ? 'active' : null ?>" href="index.php?p=admin/participacion/index">Participacion</a>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <a href="index.php?p=admin/profile_admin" class="btn btn-sm btn-outline-primary me-2">Perfil</a>
+                    <a href="pages/auth/actions/logout.php" class="btn btn-sm btn-outline-danger">Cerrar Sesión</a>
+                </div>
+                <!-- <a href="pages/auth/actions/logout.php">Cerrar Sesión</a> -->
+            <?php
+           } else {
+            ?>
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link <?php echo ($pagina == 'home') ? 'active' : null ?>" aria-current="page" href="index.php?p=home">Inicio</a>
+                    </li>
+                </ul>
+                <div class="d-flex">
+                    <a href="index.php?p=auth/login" class="btn btn-sm btn-outline-primary me-2">Iniciar Sesión</a>
+                    <a href="index.php?p=auth/register" class="btn btn-sm btn-outline-success">Registrarse</a>
+                </div>
+            <?php
+            }
+            ?>
         </div>
     </div>
 </nav>
