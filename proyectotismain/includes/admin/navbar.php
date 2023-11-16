f0ccf00848cacbde373794fb99e7f8794a852908
Rayen <rayennre@gmail.com>
2023-11-16 01:00:55 -0300

    agregar logo navbar y pag principal

--- proyectotismain/includes/admin/navbar.php
+++ proyectotismain/includes/admin/navbar.php
@@ -1,6 +1,9 @@
 <nav class="navbar navbar-expand-lg bg-body-tertiary border-bottom ">
     <div class="container-fluid">
-        <a class="navbar-brand" href="#"></a>
+        <a class="navbar-brand" href="#">
+        <img src="pages\auth\images\logo-largo.png" alt="Logo" height="40">
+        </a>
+        
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
