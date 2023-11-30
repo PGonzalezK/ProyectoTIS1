38e2e20688c7eb2b977c35f8952e11c5b8b7dec5
BigKeller <pgonzalezk@ing.ucsc.cl>
2023-11-30 20:59:20 -0300

    dirmunicipales

--- proyectotismain/proyectotis.sql
+++ proyectotismain/proyectotis.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.2.1
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 01-12-2023 a las 00:42:26
--- Versión del servidor: 10.4.28-MariaDB
--- Versión de PHP: 8.2.4
+-- Tiempo de generación: 01-12-2023 a las 00:58:28
+-- Versión del servidor: 10.4.27-MariaDB
+-- Versión de PHP: 8.1.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -56,24 +56,6 @@ INSERT INTO `asuntos` (`id`, `asunto`) VALUES
 -- --------------------------------------------------------
 
 --
--- Estructura de tabla para la tabla `backgrounds`
---
-
-CREATE TABLE `backgrounds` (
-  `id` int(11) NOT NULL,
-  `login_background` varchar(255) NOT NULL,
-  `register_background` varchar(255) NOT NULL,
-  `inicio_background` varchar(255) NOT NULL,
-  `mapa_background` varchar(255) NOT NULL,
-  `noticias_background` varchar(255) NOT NULL,
-  `eventos_background` varchar(255) NOT NULL,
-  `misionyvision_background` varchar(255) NOT NULL,
-  `participacion_background` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
-
--- --------------------------------------------------------
-
---
 -- Estructura de tabla para la tabla `categoria`
 --
 
@@ -90,7 +72,9 @@ INSERT INTO `categoria` (`id_categoria`, `nombre`) VALUES
 (1, 'Política'),
 (2, 'Deportes'),
 (3, 'Tecnología'),
-(4, 'Entretenimiento');
+(4, 'Entretenimiento'),
+(6, 'aseo y ornato'),
+(7, 'finanzas');
 
 -- --------------------------------------------------------
 
@@ -180,9 +164,14 @@ CREATE TABLE `dirmunicipales` (
 --
 
 INSERT INTO `dirmunicipales` (`id`, `nombre`, `descripcion`, `director`, `telefono`, `email`, `direccion`, `funciones`) VALUES
-(2, 'DIRECCIÓN DE DESARROLLO COMUNITARIO', 'La Dirección de Desarrollo Comunitario tiene como objetivo la promoción del desarrollo comunitario, el fortalecimiento de las organizaciones comunitarias, la proposición de medidas relacionadas con asistencia social y contingencia, la salud pública, la protección del medio ambiente, educación y cultura, capacitación laboral, deporte y recreación, promoción del empleo, fomento productivo local y turismo.', 'Paula Concha Constanzo', 2147483647, 'pconcha@concepcion.com', 'O’Higgins 525, 2° piso, Concepción', 'Asesorar al Alcalde y también al Concejo Municipal en la promoción del desarrollo comunitario.\r\nPrestar asesoría técnica a las organizaciones comunitarias, fomentar su desarrollo y legalización, y promover su efectiva participación en el municipio.\r\nProponer y ejecutar, dentro de su ámbito y cuando corresponda, medidas tendientes a materializar acciones relacionadas con la salud pública- protección del medio ambiente, educación y cultura, capacitación laboral, deportes y recreación, promoción del empleo, fomento productivo local y turismo.\r\nPrestar el servicio diseñado por el Sistema de información Social según la normativa que lo regula y proporcionar datos estadísticos y/o diagnósticos sociales para necesidades generales de la Dideco o de la Municipalidad.\r\nRealizar los procesos pertinentes en la gestión de abastecimiento, según las normas de la Plataforma Mercado Público y Reglamentos de Adquisiciones vigentes en el municipio.'),
-(4, 'a', '', '', 0, 'admin@correo.com', '', ''),
-(5, 'a', '', '', 0, 'admin@correo.com', '', '');
+(2, 'DIRECCIÓN DE DESARROLLO COMUNITARIO', 'La Dirección de Desarrollo Comunitario tiene como objetivo la promoción del desarrollo comunitario, el fortalecimiento de las organizaciones comunitarias, la proposición de medidas relacionadas con asistencia social y contingencia, la salud pública, la protección del medio ambiente, educación y cultura, capacitación laboral, deporte y recreación, promoción del empleo, fomento productivo local y turismo.', 'Paula Concha Constanzo', 412266590, 'pconcha@concepcion.com', 'O’Higgins 525, 2° piso, Concepción', 'Asesorar al Alcalde y también al Concejo Municipal en la promoción del desarrollo comunitario.\r\nPrestar asesoría técnica a las organizaciones comunitarias, fomentar su desarrollo y legalización, y promover su efectiva participación en el municipio.\r\nProponer y ejecutar, dentro de su ámbito y cuando corresponda, medidas tendientes a materializar acciones relacionadas con la salud pública- protección del medio ambiente, educación y cultura, capacitación laboral, deportes y recreación, promoción del empleo, fomento productivo local y turismo.\r\nPrestar el servicio diseñado por el Sistema de información Social según la normativa que lo regula y proporcionar datos estadísticos y/o diagnósticos sociales para necesidades generales de la Dideco o de la Municipalidad.\r\nRealizar los procesos pertinentes en la gestión de abastecimiento, según las normas de la Plataforma Mercado Público y Reglamentos de Adquisiciones vigentes en el municipio.'),
+(6, ' Dirección de Aseo y Ornato', 'La Dirección de Aseo, Ornato y Medio Ambiente tiene como objetivo, velar por el aseo y orden de los espacios públicos de la comuna, proporcionando una adecuada y eficiente extracción y disposición de los residuos domiciliarios, así como también la administración, ornamentación y mantención de las áreas verdes, todo lo anterior en contribución al mejoramiento del medio ambiente, haciendo de Concepción una comuna sustentable.', 'Mauricio Talpen Sanhueza', 412263026, 'mtalpen@concepcion.cl', 'Ejército 1058, Concepción', 'Diseñar programas para la disposición final de la basura.\r\nOrganizar el aseo de vías públicas, la mantención de parques, jardines y en general, el aseo de los bienes nacionales de uso público existentes en la comuna.\r\nAdministrar las áreas verdes de la comuna y proponer programas de construcción, reparación, mantención y cuidado de éstas.\r\nEvaluar permanentemente los procedimientos empleados en su unidad, de manera tal, que éstos se encuentren actualizados y estén acordes con una buena gestión.\r\nControlar el rendimiento y la eficiencia en las labores del personal de su dependencia.\r\nControlar la correcta disposición final de la basura inerte que se deposite en el vertedero.\r\nColaborar en la protección y defensa de las áreas verdes públicas de la comuna y en la elaboración de proyectos de ornamentación.\r\nApoyar a la solución de problemas medioambientales de la Comuna\r\nControlar el retiro oportuno de escombros y micro basurales, denunciando ante la autoridad sanitaria correspondiente para la iniciación de los procesos que fuesen pertinentes.\r\nDisponer el control y correcto funcionamiento del equipo rodante que efectúa labores de aseo y recolección de basuras.\r\nOtras funciones que impliquen desarrollar acciones tendientes a la solución de problemas que afecten a la comunidad en el área de su competencia.\r\nRecepcionar áreas verdes producto de loteos habitacionales.\r\nRecepcionar áreas verdes construidas por otros organismos públicos.'),
+(7, 'DIRECCIÓN DE INFORMÁTICA', 'Desarrollar un plan informático para el municipio y asesorar a todas las unidades en el área de su competencia. Administrar los recursos informáticos para dar un servicio eficiente a las unidades municipales que lo requieran.', 'Claudio Letelier Segovia', 412266660, 'cletelier@concepcion.cl', 'O’Higgins 525, 4° piso, Concepción.', 'Efectuar la planificación informática de la municipalidad y evaluar periódicamente los procedimientos empleados en su ejecución.\r\nDefinir y priorizar los Proyectos Informáticos cuando procediere.\r\nSupervisar los contratos con empresas de servicios computacionales.\r\nApoyar a otras Direcciones en temas de su competencia.\r\nDefinir las normas y estándares informáticos.\r\nProponer las políticas de adquisición de hardware, software y servicios.\r\nEvaluar permanentemente los procedimientos empleados en su dirección de manera tal que éstos se encuentren actualizados y estén acordes con una buena gestión.\r\nMedir la eficiencia de las labores del personal de su unidad.'),
+(8, 'Secretaria Comunal de Planificación y Coordinación', 'La Secretaría Comunal de Planificación y Coordinación tiene como objetivo asesorar al Alcalde y al Concejo Municipal, en la formulación de las estrategias comunales y municipales para lograr el desarrollo comunal.', 'Pedro Venegas Castro', 412266710, 'pvenegas@concepcion.cl', 'O’Higgins 525, 6° piso, Concepción', 'Servir de secretaría técnica permanente del Alcalde y del Concejo Municipal en la preparación y coordinación de las políticas, planes, programas y proyectos de desarrollo de la comuna.\r\nAsesorar al Alcalde, en coordinación con la Dirección de Finanzas, en la elaboración del proyecto de Presupuesto Municipal.\r\nEvaluar el cumplimiento de planes, programas, proyectos, inversiones y presupuesto municipal e informar sobre estas materias al Alcalde y al Concejo Municipal, al menos semestralmente.\r\nEfectuar análisis y evaluaciones permanentes de la situación de desarrollo de la comuna, con énfasis en los aspectos sociales y territoriales.\r\nFomentar vinculaciones técnicas con los servicios públicos y con el sector privado de la comuna.\r\nRecopilar y mantener la información comunal y regional atingente a sus funciones.\r\nElaborar las Bases Generales y Específicas para los llamados a licitación, previo informe de la Unidad competente, de conformidad con los criterios e instrucciones establecidos en el reglamento municipal respectivo.\r\nElaborar y mantener actualizado el Plan de Desarrollo Comunal, incluyendo los aspectos sociales, territoriales, económicos y presupuestarios correspondientes.\r\nProponer las medidas que se requiera adoptar para impulsar el desarrollo social y la mejor adecuación del medio físico, tendientes al bienestar general de la población.\r\nAsesorar técnicamente y proporcionar la información que corresponda a las unidades municipales, a los servicios públicos de la comuna, a las organizaciones intermedias y al público que así lo requiera, en las materias propias de su competencia.\r\nProgramar el trabajo anual en materias de proyectos, en conjunto con las respectivas direcciones municipales.\r\nEfectuar el seguimiento y la coordinación de los proyectos que el municipio está postulando a cualquier fuente de financiamiento y de la ejecución de los mismos en sus avances físicos y financieros.\r\nMantener un banco de proyectos de todos los estudios e inversiones del Municipio.\r\nProcesar, analizar e informar sobre la marcha presupuestaria y financiera del municipio, en forma periódica.\r\nElaborar y proponer con otras direcciones municipales, los proyectos y programas específicos en las áreas territoriales y sociales de la comuna.'),
+(9, 'DIRECCIÓN DE SEGURIDAD PÚBLICA', 'Contribuir a incrementar los niveles de seguridad de la ciudad, previniendo actos de violencia e incivilidades, en particular respecto de los grupos más vulnerables, Fortalecer la organización comunitaria a través de la corresponsabilidad social y la coproducción de seguridad y Vincular a la comunidad con redes intra e interinstitucionales del ámbito de seguridad.', 'Daisy Cardenas', 412263055, 'dcardenas@concepcion.cl', 'Ejército #1020, Concepción', 'Colaborar directamente con el alcalde en las tareas de implementación, evaluación, promoción, capacitación y apoyo de acciones de prevención social y situacional, la celebración de convenios con otras entidades públicas para la aplicación de planes de reinserción social y de asistencia a víctimas, así como también la adopción de medidas en el ámbito de la seguridad pública a nivel comunal.\r\n\r\nColaborar directamente con el alcalde en la elaboración, aprobación, ejecución y evaluación del Plan Comunal de Seguridad Pública.\r\n\r\nPreparar información y proponer políticas, estudios, proyectos y programas en materia de seguridad pública.\r\n\r\nEstablecer mesas de trabajo vinculadas a la seguridad pública y seguridad ciudadana.\r\n\r\nAdministrar los contratos relacionados con servicios de seguridad pública y seguridad ciudadana.'),
+(10, 'DIRECCIÓN JURÍDICA', 'Su objetivo será el de prestar apoyo en materias legales al alcalde y al concejo, informar en derecho todos los asuntos legales que las distintas unidades le planteen y orientarlas periódicamente en los temas de su área y asumir, a requerimiento del alcalde, la defensa municipal.', 'Adolfo Muñoz Estrada', 412266750, 'amunoz@concepcion.cl', 'O´Higgins 525, 5° piso', 'Estudiar y mantener un registro actualizado de los títulos correspondientes a los bienes raíces municipales.\r\nResolver consultas de las diferentes Direcciones y Departamentos que requieran de sus servicios.\r\nDefinir procedimientos empleados en la dirección para procurar mejorar la calidad de gestión.\r\nMedir la eficiencia de las labores del personal de su unidad mediante la respectiva calificación.'),
+(11, 'DIRECCIÓN DE RELACIONES PÚBLICAS Y PRENSA', 'Desarrollar una gestión eficiente e integral de las comunicaciones entre la comunidad y la municipalidad. Gestión de Contenidos: coordina las Unidades de Prensa, Redes Sociales, Audiovisual y Medios Corporativos. Producción y Relaciones Públicas: coordina el quehacer de las Unidades de Relaciones Públicas, Producción de ceremonias y eventos y Marketing.', 'Sergio Torres Sepúlveda ', 412266680, 'storres@concepcion.cl', 'O’Higgins 525, 6° piso, Concepción ', 'Coordinar la ejecución del Plan Estratégico de Comunicaciones.\r\nMantener oportunamente informada a la comunidad sobre los servicios, actividades municipales, emergencias locales y otras materias de interés, a través de los distintos soportes y plataformas propios y/o externos que sean pertinentes.\r\nMonitorear e informar periódicamente al alcalde los planteamientos relacionados con la administración de la comuna que se difundan por los medios de comunicación y redes sociales.\r\nAsesorar al alcalde y directores en materia de gestión de medios y crisis comunicacionales, desarrollo de la imagen de ciudad y campañas públicas, relaciones públicas y actividades protocolares.\r\nPlanificar coordinar y ejecutar los diferentes programas municipales de su dirección y lograr el máximo de difusión a través de plan de medios y campaña comunicacional territorial de la imagen municipal, actividades y eventos.\r\nPlanificar, coordinar y ejecutar diferentes actividades municipales, ya sea producción de eventos artísticos, culturales y deportivos, además de ceremonias e inauguraciones de obras y proyectos.\r\nPlanificar, coordinar y ejecutar las diversas actividades protocolares y ceremoniales que realiza oficialmente el municipio (invitaciones, programa, libreto, entre otras acciones).\r\nCoordinar el uso diario del Salón de Honor Regidor Carlos Contreras Maluje para las diferentes actividades municipales.'),
+(12, 'DIRECCIÓN DE CULTURA', 'Somos la unidad encargada de implementar las políticas culturales municipales, definidas de forma participativa. Trabajamos por el desarrollo cultural de Concepción, fomentando la vinculación de las vecinas y vecinos con las Culturas, las Artes y la Creatividad, fortaleciendo la identidad penquista y promoviendo la Cultura como un derecho, cuarto pilar del desarrollo sostenible en la comuna.', 'Mauricio Castro Rivas', 412266687, 'mcastro@concepcion.cl', 'O’Higgins 525, subterráneo, Concepción', 'Trabajamos para garantizar el derecho a la cultura en la ciudad, facilitando el acceso al capital cultural y fomentando los hábitos y prácticas culturales.\r\nOrientamos en las distintas etapas del desarrollo de proyectos culturales: formulación, formación, creación, difusión, exhibición, vinculación, ya sean independientes, organizacionales, profesionales o aficionados.\r\nCoordinamos permisos para el desarrollo de actividades culturales en espacios públicos e infraestructuras municipales, además de coordinar con otros servicios municipales que se requieran.\r\nDifundimos el quehacer cultural de la comuna por medio de plataformas digitales y análogas.\r\nFinanciamos actividades a través de un fondo concursable (FAICC).\r\nGeneramos redes de trabajo colaborativo sectorial e institucional.');
 
 -- --------------------------------------------------------
 
@@ -253,11 +242,11 @@ CREATE TABLE `mapa` (
 --
 
 INSERT INTO `mapa` (`id_mapa`, `email`, `nombre_punto`, `descripcion`, `direccion`, `aprobado`) VALUES
-(1, 'pmonjes@ing.ucsc.cl', 'Barberia', 'a', 'aaa #1', 1),
-(2, 'pmonjes@ing.ucsc.cl', 'asd', 'asd', 'aaa #2', 1),
-(4, 'pmonjes@ing.ucsc.cl', 'cs', 'sd', 'aaa #3', 0),
-(5, 'pmonjes@ing.ucsc.cl', 'dd', 'dd', 'aaa #4', 0),
-(6, 'pmonjes@ing.ucsc.cl', 'c', 'cc', 'aaa #5', 1);
+(1, 'pmonjes@ing.ucsc.cl', 'Barberia', 'cortes de pelo personalizados', 'prat #1690', 1),
+(2, 'pmonjes@ing.ucsc.cl', 'confiteria', 'ventas de dulces y frutos secos', 'maipu poniente #44', 1),
+(4, 'pmonjes@ing.ucsc.cl', 'muebleria', 'muebles a pedidos y prefabricados', 'paicavi #3556', 1),
+(5, 'pmonjes@ing.ucsc.cl', 'supermarket elixir', 'venta de insumos de hogar.', 'san martin #433', 0),
+(6, 'pmonjes@ing.ucsc.cl', 'panaderia el pilar', 'amasanderia y panaderia el pilar', 'orompello #5421', 1);
 
 -- --------------------------------------------------------
 
@@ -454,12 +443,6 @@ ALTER TABLE `asuntos`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indices de la tabla `backgrounds`
---
-ALTER TABLE `backgrounds`
-  ADD PRIMARY KEY (`id`);
-
---
 -- Indices de la tabla `categoria`
 --
 ALTER TABLE `categoria`
@@ -576,16 +559,10 @@ ALTER TABLE `valorizaciones`
 --
 
 --
--- AUTO_INCREMENT de la tabla `backgrounds`
---
-ALTER TABLE `backgrounds`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
 -- AUTO_INCREMENT de la tabla `categoria`
 --
 ALTER TABLE `categoria`
-  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id_categoria` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT de la tabla `comentarios`
@@ -609,7 +586,7 @@ ALTER TABLE `denuncias`
 -- AUTO_INCREMENT de la tabla `dirmunicipales`
 --
 ALTER TABLE `dirmunicipales`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
 
 --
 -- AUTO_INCREMENT de la tabla `emprendedores`
