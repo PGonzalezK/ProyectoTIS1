1e125487e1bd5904eb8df988587ec386f0edc53d
Rayen <rayennre@gmail.com>
2023-12-01 00:20:21 -0300

    eventos update

--- proyectotismain/pages/admin/eventos_adm/templates/mainEventos.php
+++ proyectotismain/pages/admin/eventos_adm/templates/mainEventos.php
@@ -14,7 +14,7 @@
             <div class="tn-img">
                 <img src="pages/admin/eventos_adm/imagenes/<?php echo $evento['imagen']; ?>" alt="">
                 <div class="tn-title">
-                <a href="index.php?p=actualidad/eventos/verEvento&id=<?php echo $evento['idEvento']; ?>"><?php echo $evento['titulo']; ?></a>
+                <a href="index.php?p=actualidad/eventos/verEvento&id=<?php echo $evento['idEvento']; ?>"><?php echo $evento['titulo'] . '<br>' . $evento['direccion']; ?></a>
                 </div>
             </div>
         </div>
